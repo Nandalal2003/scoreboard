9ebabc918da43ad2431eebc99738b409e780a10d
NANDALAL P N <115664139+Nandalal2003@users.noreply.github.com>
2026-01-17 22:55:26 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,40 @@
+let count_second=0
+let count_first=0
+
+function count11(){
+	count_second=count_second+1
+	document.getElementById("second-span").innerText=count_second
+
+
+}
+function count22(){
+	count_second=count_second+2
+	document.getElementById("second-span").textContent=count_second
+
+
+}
+function count33(){
+	count_second=count_second+3
+	document.getElementById("second-span").textContent=count_second
+
+
+}
+
+function count1(){
+	count_first=count_first+1
+	document.getElementById("first-span").innerText=count_first
+
+
+}
+function count2(){
+	count_first=count_first+2
+	document.getElementById("first-span").textContent=count_first
+
+
+}
+function count3(){
+	count_first=count_first+3
+	document.getElementById("first-span").textContent=count_first
+
+
+}
